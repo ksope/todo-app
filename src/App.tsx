2911f9897cf0256a00c802850c4635d8e756f8ef
ksope <105855785+ksope@users.noreply.github.com>
2024-04-02 20:04:33 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./App.css";
+
+
+function App ():React.ReactNode{
+    return <div className="App">
+      <span className="heading">Taskify</span>
+      </div>;
+}
+
+export default App;
